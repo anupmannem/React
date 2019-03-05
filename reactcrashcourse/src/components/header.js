c8206bdb98323c6fe023b0717d0cd56f752379e2
anupmannem <anupmannem@gmail.com>
2019-03-05 12:07:25 +0530

    react events tested

--- reactcrashcourse/src/components/header.js
+++ reactcrashcourse/src/components/header.js
@@ -1,14 +1,19 @@
 // imports
 import React, { Component } from 'react';
-import CSS from "../css/style.css";
+import  "../css/style.css";
 
 // class component
 class Header extends Component {
+    // react event    
+    onClickHandler(e) {
+        console.log(e.target.value);        
+    }    
     render() {
         return (
             <header>
                 <div className="logo"> Logo </div>
-                <input type="text" />
+                {/* <input type="text" onClick={this.onClickHandler} /> */}
+                <input type="text" onChange={this.onClickHandler} />                          
             </header>
         )
     }
