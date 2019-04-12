d182a5d37e8ebcde1ef5650838f7e021b3427774
anupmannem <anupmannem@gmail.com>
2019-04-12 14:44:47 +0530

    links added to router

--- react-router/src/App.js
+++ react-router/src/App.js
@@ -1,16 +1,38 @@
 import React, { Component } from "react";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Link } from "react-router-dom";
 
 import Home from "./components/Home";
 import Profile from "./components/Profile";
 import Posts from "./components/Posts";
 import Contact from "./components/Contact";
 
+class Header extends React.Component {
+  render() {
+    return (
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/profile">Profile</Link>
+        </li>
+        <li>
+          <Link to="/posts">Posts</Link>
+        </li>
+        <li>
+          <Link to="/contact">Contact</Link>
+        </li>
+      </ul>
+    );
+  }
+}
+
 class App extends Component {
   render() {
     return (
       <div>
         <BrowserRouter>
+          <Header />
           <div>
             <Route path="/" exact component={Home} />
             <Route path="/profile" exact component={Profile} />
